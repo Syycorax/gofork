842062e364b6f3ea6b13f989fae959a15c1275d7
Syycorax <54723548+Syycorax@users.noreply.github.com>
2022-01-22 00:01:27 +0100

    implemented state of fork

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
-module github.com/Syycorax/gofork
+module github.com/syycorax/gofork
 
 go 1.17
 
-require (
-	github.com/buger/jsonparser v1.1.1
-	github.com/gookit/color v1.5.0
-)
+require github.com/gookit/color v1.5.0
 
 require (
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
--- gofork.go
+++ gofork.go
@@ -12,7 +12,7 @@ import (
 
 type Repo_info struct {
 	Fork_count int `json:"forks_count"`
-	login      Owner
+	Owner      Owner
 }
 
 type Owner struct {
@@ -24,14 +24,21 @@ type Fork struct {
 	Compare_url string `json:"compare_url"`
 	Status      string `json:"status"`
 	Ahead_by    int    `json:"ahead_by"`
+	Behind_by   int    `json:"behind_by"`
+}
+
+type Auth struct {
+	Token string `json:"PAT"`
 }
 
 func main() {
 	var (
 		repo_info Repo_info
 		forks     []Fork
+		auth      Auth
 	)
-
+	dat, _ := os.ReadFile("./config.json")
+	json.Unmarshal([]byte(dat), &auth)
 	fail := "[X]"
 	success := "[✓]"
 	working := "[+]"
@@ -39,9 +46,12 @@ func main() {
 	fmt.Println(working + " Looking for " + repo)
 	url := "https://api.github.com/repos/" + repo
 	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", "token "+string(auth.Token))
 	resp, _ := http.DefaultClient.Do(req)
 	if resp.StatusCode == http.StatusNotFound {
 		color.Red.Println(fail + " Repository not found")
+	} else if resp.StatusCode == http.StatusForbidden {
+		color.Red.Println(fail + " No PAT")
 	} else {
 		color.Green.Println(success + " Repository found")
 		body, _ := ioutil.ReadAll(resp.Body)
@@ -49,16 +59,29 @@ func main() {
 		if repo_info.Fork_count == 0 {
 			color.Red.Println(fail + " No forks found")
 		} else {
-			fmt.Println(repo_info.login)
 			color.Green.Println(success, repo_info.Fork_count, "Forks found")
 			url = "https://api.github.com/repos/" + repo + "/forks"
 			req, _ = http.NewRequest("GET", url, nil)
+			req.Header.Add("Authorization", "token "+string(auth.Token))
 			resp, _ = http.DefaultClient.Do(req)
 			body, _ = ioutil.ReadAll(resp.Body)
 			json.Unmarshal(body, &forks)
 			for _, fork := range forks {
-				url = "https://api.github.com/repos/" + fork.Full_name + "/compare/master...develop"
-				fmt.Println(fork.Full_name)
+				url = "https://api.github.com/repos/" + fork.Full_name + "/compare/" + repo_info.Owner.Login + ":master...master"
+				req, _ = http.NewRequest("GET", url, nil)
+				req.Header.Add("Authorization", "token "+string(auth.Token))
+				resp, _ = http.DefaultClient.Do(req)
+				body, _ = ioutil.ReadAll(resp.Body)
+				json.Unmarshal(body, &fork)
+				if fork.Status == "ahead" {
+					color.Green.Println(success, fork.Full_name, "ahead by", fork.Ahead_by, "commits")
+				} else if fork.Status == "behind" {
+					color.Red.Println(fail, fork.Full_name, "behind by", fork.Behind_by, "commits")
+				} else if fork.Status == "identical" {
+					color.Yellow.Println(fail, fork.Full_name, "up to date")
+				} else {
+					color.Blue.Println(fail, fork.Full_name, "Fork is private")
+				}
 			}
 		}
 	}
