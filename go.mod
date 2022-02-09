77e975d766d3cbd20f6c3ce8a89f4b588f182343
Syycorax <54723548+Syycorax@users.noreply.github.com>
2022-02-10 00:19:43 +0100

    added progress bar

--- go.mod
+++ go.mod
@@ -8,8 +8,13 @@ require (
 )
 
 require (
-	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/schollz/progressbar/v3 v3.8.6 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+	golang.org/x/crypto v0.0.0-20220209195652-db638375bc3a // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
--- gofork.go
+++ gofork.go
@@ -13,12 +13,13 @@ import (
 	"github.com/akamensky/argparse"
 	"github.com/gookit/color"
 	"github.com/olekukonko/tablewriter"
+	"github.com/schollz/progressbar/v3"
 )
 
-type Repo_info struct {
-	Fork_count     int `json:"forks_count"`
-	Owner          Owner
-	Default_branch string `json:"default_branch"`
+type RepoInfo struct {
+	ForkCount     int `json:"forks_count"`
+	Owner         Owner
+	DefaultBranch string `json:"default_branch"`
 }
 
 type Owner struct {
@@ -26,11 +27,11 @@ type Owner struct {
 }
 
 type Fork struct {
-	Full_name string `json:"full_name"`
-	Url       string `json:"html_url"`
-	Status    string `json:"status"`
-	Ahead_by  int    `json:"ahead_by"`
-	Behind_by int    `json:"behind_by"`
+	FullName string `json:"full_name"`
+	Url      string `json:"html_url"`
+	Status   string `json:"status"`
+	AheadBy  int    `json:"ahead_by"`
+	BehindBy int    `json:"behind_by"`
 }
 
 type Auth struct {
@@ -39,18 +40,18 @@ type Auth struct {
 
 func main() {
 	var (
-		repo_info Repo_info
-		forks     []Fork
-		auth      Auth
+		RepoInfo RepoInfo
+		forks    []Fork
+		auth     Auth
 	)
 	fail := "[X]"
 	success := "[✓]"
 	working := "[+]"
 	mitigate := "[~]"
 	parser := argparse.NewParser("gofork", "CLI tool to find active forks")
-	repo := parser.String("r", "repo", &argparse.Options{Required: true, Help: "Repository to check"})
+	repo := parser.String("r", "repo", &argparse.Options{Required: true, Help: "Repository to check"}) //TODO: No flag just default arg
 	branch := parser.String("b", "branch", &argparse.Options{Required: false, Help: "Branch to check", Default: "repo default branch"})
-	privateflag := parser.Flag("p", "private", &argparse.Options{Help: "Show private repositories"})
+	verboseflag := parser.Flag("v", "verbose", &argparse.Options{Help: "Show private and up to date repositories"})
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
@@ -79,23 +80,23 @@ func main() {
 	} else {
 		color.Success.Println(success + " Repository found")
 		body, _ := ioutil.ReadAll(resp.Body)
-		json.Unmarshal(body, &repo_info)
+		json.Unmarshal(body, &RepoInfo)
 		if *branch == "repo default branch" {
-			*branch = repo_info.Default_branch
+			*branch = RepoInfo.DefaultBranch
 		}
 		color.Notice.Println(working + " Looking for " + *repo + ":" + *branch)
-		if repo_info.Fork_count == 0 {
+		if RepoInfo.ForkCount == 0 {
 			if platform == "windows" {
 				color.Error.Print(fail + " No forks found")
 			} else {
 				color.Error.Print(fail + " No forks found \n")
 			}
 		} else {
-			color.Success.Println(success, repo_info.Fork_count, "Forks found")
-			if repo_info.Fork_count > 100 {
-				color.Info.Println(mitigate + " More than 100 forks found, only showing first 100")
+			color.Success.Println(success, RepoInfo.ForkCount, "Forks found")
+			if RepoInfo.ForkCount > 100 {
+				color.Info.Println(mitigate + " More than 100 forks found, only showing first 100") //TODO: Add pagination
 			}
-			url = "https://api.github.com/repos/" + *repo + "/forks?per_page=" + strconv.Itoa(repo_info.Fork_count)
+			url = "https://api.github.com/repos/" + *repo + "/forks?per_page=" + strconv.Itoa(RepoInfo.ForkCount)
 			req, _ = http.NewRequest("GET", url, nil)
 			req.Header.Add("Authorization", "token "+string(auth.Token))
 			resp, _ = http.DefaultClient.Do(req)
@@ -106,12 +107,13 @@ func main() {
 			diverge := list.New()
 			even := list.New()
 			private := list.New()
+			bar := progressbar.Default(int64(RepoInfo.ForkCount))
 			for _, fork := range forks {
-				url = "https://api.github.com/repos/" + fork.Full_name + "/compare/" + repo_info.Owner.Login + ":" + *branch + "..." + *branch
+				url = "https://api.github.com/repos/" + fork.FullName + "/compare/" + RepoInfo.Owner.Login + ":" + *branch + "..." + *branch
 				req, _ = http.NewRequest("GET", url, nil)
 				req.Header.Add("Authorization", "token "+string(auth.Token))
 				resp, _ = http.DefaultClient.Do(req)
-				body, _ = ioutil.ReadAll(resp.Body)
+				body, _ = ioutil.ReadAll(resp.Body)	
 				json.Unmarshal(body, &fork)
 				if fork.Status == "ahead" {
 					ahead.PushBack(fork)
@@ -124,16 +126,16 @@ func main() {
 				} else {
 					private.PushBack(fork)
 				}
+				bar.Add(1)
 			}
 			aheadtable := tablewriter.NewWriter(os.Stdout)
 			aheadtable.SetHeader([]string{"Fork", "Ahead by", "URL"})
 			aheadmap := [][]string{}
-
 			for e := ahead.Front(); e != nil; e = e.Next() {
 				fork := e.Value.(Fork)
-				ahead_by := strconv.Itoa(fork.Ahead_by)
-				url := "https://github.com/" + string(fork.Full_name)
-				aheadmap = append(aheadmap, []string{fork.Full_name, ahead_by, url})
+				aheadBy := strconv.Itoa(fork.AheadBy)
+				url := "https://github.com/" + string(fork.FullName)
+				aheadmap = append(aheadmap, []string{fork.FullName, aheadBy, url})
 
 			}
 			for _, v := range aheadmap {
@@ -143,35 +145,17 @@ func main() {
 				color.Success.Println(success + " Forks ahead:")
 				aheadtable.Render()
 			} else {
-				color.Notice.Println(mitigate + " No forks ahead of " + repo_info.Owner.Login + ":" + *branch)
-			}
-			behindtable := tablewriter.NewWriter(os.Stdout)
-			behindtable.SetHeader([]string{"Fork", "Behind by", "URL"})
-			behindmap := [][]string{}
-			for e := behind.Front(); e != nil; e = e.Next() {
-				fork := e.Value.(Fork)
-				behind_by := strconv.Itoa(fork.Behind_by)
-				url := "https://github.com/" + string(fork.Full_name)
-				behindmap = append(behindmap, []string{fork.Full_name, behind_by, url})
-			}
-			for _, v := range behindmap {
-				behindtable.Append(v)
-			}
-			if behind.Len() > 0 {
-				color.Warn.Println(fail + " Forks behind:")
-				behindtable.Render()
-			} else {
-				color.Notice.Println(mitigate + " No forks behind of " + repo_info.Owner.Login + ":" + *branch)
+				color.Notice.Println(mitigate + " No forks ahead of " + RepoInfo.Owner.Login + ":" + *branch)
 			}
 			divergetable := tablewriter.NewWriter(os.Stdout)
 			divergetable.SetHeader([]string{"Fork", "Ahead by", "Behind by", "URL"})
 			divergemap := [][]string{}
 			for e := diverge.Front(); e != nil; e = e.Next() {
 				fork := e.Value.(Fork)
-				ahead_by := strconv.Itoa(fork.Ahead_by)
-				behind_by := strconv.Itoa(fork.Behind_by)
-				url := "https://github.com/" + string(fork.Full_name)
-				divergemap = append(divergemap, []string{fork.Full_name, ahead_by, behind_by, url})
+				aheadBy := strconv.Itoa(fork.AheadBy)
+				behindBy := strconv.Itoa(fork.BehindBy)
+				url := "https://github.com/" + string(fork.FullName)
+				divergemap = append(divergemap, []string{fork.FullName, aheadBy, behindBy, url})
 			}
 			for _, v := range divergemap {
 				divergetable.Append(v)
@@ -180,33 +164,51 @@ func main() {
 				color.Notice.Println(mitigate + " Forks diverged:")
 				divergetable.Render()
 			} else {
-				color.Notice.Println(mitigate + " No forks diverged of " + repo_info.Owner.Login + ":" + *branch)
+				color.Notice.Println(mitigate + " No forks diverged of " + RepoInfo.Owner.Login + ":" + *branch)
 			}
-			eventable := tablewriter.NewWriter(os.Stdout)
-			eventable.SetHeader([]string{"Fork", "URL"})
-			eventmap := [][]string{}
-			for e := even.Front(); e != nil; e = e.Next() {
+			behindtable := tablewriter.NewWriter(os.Stdout)
+			behindtable.SetHeader([]string{"Fork", "Behind by", "URL"})
+			behindmap := [][]string{}
+			for e := behind.Front(); e != nil; e = e.Next() {
 				fork := e.Value.(Fork)
-				url := "https://github.com" + string(fork.Full_name)
-				eventmap = append(eventmap, []string{fork.Full_name, url})
+				behindBy := strconv.Itoa(fork.BehindBy)
+				url := "https://github.com/" + string(fork.FullName)
+				behindmap = append(behindmap, []string{fork.FullName, behindBy, url})
 			}
-			for _, v := range eventmap {
-				eventable.Append(v)
+			for _, v := range behindmap {
+				behindtable.Append(v)
 			}
-			if even.Len() > 0 {
-				color.Notice.Println(mitigate + " Forks up to date:")
-				eventable.Render()
+			if behind.Len() > 0 {
+				color.Warn.Println(fail + " Forks behind:")
+				behindtable.Render()
 			} else {
-				color.Notice.Println(mitigate + " No forks identical to " + repo_info.Owner.Login + ":" + *branch)
+				color.Notice.Println(mitigate + " No forks behind of " + RepoInfo.Owner.Login + ":" + *branch)
 			}
-			if *privateflag {
+			if *verboseflag {
+				eventable := tablewriter.NewWriter(os.Stdout)
+				eventable.SetHeader([]string{"Fork", "URL"})
+				eventmap := [][]string{}
+				for e := even.Front(); e != nil; e = e.Next() {
+					fork := e.Value.(Fork)
+					url := "https://github.com" + string(fork.FullName)
+					eventmap = append(eventmap, []string{fork.FullName, url})
+				}
+				for _, v := range eventmap {
+					eventable.Append(v)
+				}
+				if even.Len() > 0 {
+					color.Notice.Println(mitigate + " Forks up to date:")
+					eventable.Render()
+				} else {
+					color.Notice.Println(mitigate + " No forks identical to " + RepoInfo.Owner.Login + ":" + *branch)
+				}
 				privatetable := tablewriter.NewWriter(os.Stdout)
 				privatetable.SetHeader([]string{"Fork", "URL"})
 				privatemap := [][]string{}
 				for e := private.Front(); e != nil; e = e.Next() {
 					fork := e.Value.(Fork)
-					url := "https://github.com" + string(fork.Full_name)
-					privatemap = append(privatemap, []string{fork.Full_name, url})
+					url := "https://github.com" + string(fork.FullName)
+					privatemap = append(privatemap, []string{fork.FullName, url})
 				}
 				for _, v := range privatemap {
 					privatetable.Append(v)
@@ -215,7 +217,7 @@ func main() {
 					color.Question.Println(mitigate + " Private forks:")
 					privatetable.Render()
 				} else {
-					color.Notice.Println(mitigate + " No forks private of " + repo_info.Owner.Login + ":" + *branch)
+					color.Notice.Println(mitigate + " No forks private of " + RepoInfo.Owner.Login + ":" + *branch)
 				}
 			}
 			if ahead.Len() == 0 && behind.Len() == 0 && even.Len() == 0 && *branch == "master" {
